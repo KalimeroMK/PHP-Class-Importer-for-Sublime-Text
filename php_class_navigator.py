3d33875c3718bf2582c63c9e4f6d350f7858e804
KalimeroMK <zbogoevski@gmail.com>
2025-05-22 21:38:51 +0200

    PHP Class Navigator loaded (Python 3.3 compatible)

--- php_class_navigator.py
+++ php_class_navigator.py
@@ -7,7 +7,6 @@ from functools import lru_cache
 
 class PhpClassNavigator(sublime_plugin.EventListener):
     def on_hover(self, view, point, hover_zone):
-        """Show visual feedback for clickable classes"""
         if hover_zone != sublime.HOVER_TEXT:
             return
 
@@ -21,7 +20,6 @@ class PhpClassNavigator(sublime_plugin.EventListener):
             )
 
     def on_text_command(self, view, command_name, args):
-        """Handle ⌘+Click to open classes"""
         if (command_name == "drag_select" and
             args.get("by") == "words" and
             sublime.get_mouse_additional_buttons() & sublime.MOUSE_CMD):
@@ -32,45 +30,50 @@ class PhpClassNavigator(sublime_plugin.EventListener):
                 view.run_command("dynamic_class_search_and_import", {
                     "class_name": class_name
                 })
-                return ("noop", None)  # Cancel default selection
+                return ("noop", None)
 
 class DynamicClassSearchAndImportCommand(sublime_plugin.TextCommand):
     def run(self, edit, class_name=None):
         if not class_name:
             class_name = self.get_selection()
             if not class_name:
-                return sublime.status_message("No class name selected")
+                sublime.status_message("No class name selected")
+                return
 
-        if project_dir := self.get_project_root():
-            threading.Thread(
-                target=self.find_and_handle_class,
-                args=(project_dir, class_name.strip())
-            ).start()
-        else:
+        project_dir = self.get_project_root()
+        if not project_dir:
             sublime.status_message("No project directory found")
+            return
+
+        threading.Thread(
+            target=self.find_and_handle_class,
+            args=(project_dir, class_name.strip())
+        ).start()
 
     def get_project_root(self):
-        if folders := sublime.active_window().folders():
-            return folders[0]
-        return None
+        folders = sublime.active_window().folders()
+        return folders[0] if folders else None
 
     def get_selection(self):
         for region in self.view.sel():
-            return (self.view.substr(region) if not region.empty()
-                    else self.view.substr(self.view.word(region)))
+            if not region.empty():
+                return self.view.substr(region)
+            return self.view.substr(self.view.word(region))
         return None
 
     def find_and_handle_class(self, project_dir, class_name):
-        if class_info := self.find_php_class(project_dir, class_name):
+        class_info = self.find_php_class(project_dir, class_name)
+        if class_info:
             sublime.set_timeout(
                 lambda: self.open_class_file(class_info[1]), 0)
         else:
             sublime.set_timeout(
-                lambda: sublime.status_message(f"Class not found: {class_name}"), 0)
+                lambda: sublime.status_message("Class not found: %s" % class_name), 0)
 
     @lru_cache(maxsize=100)
     def find_php_class(self, project_dir, class_name):
-        return self.build_class_map(project_dir).get(class_name)
+        class_map = self.build_class_map(project_dir)
+        return class_map.get(class_name)
 
     def build_class_map(self, directory):
         class_map = {}
@@ -80,11 +83,9 @@ class DynamicClassSearchAndImportCommand(sublime_plugin.TextCommand):
                     full_path = os.path.join(root, file)
                     classes = self.extract_classes_from_file(full_path)
                     for cls_name, namespace in classes:
-                        fqcn = f"{namespace}\\{cls_name}" if namespace else cls_name
-                        class_map.update({
-                            cls_name: (fqcn, full_path),
-                            fqcn: (fqcn, full_path)
-                        })
+                        fqcn = "%s\\%s" % (namespace, cls_name) if namespace else cls_name
+                        class_map[cls_name] = (fqcn, full_path)
+                        class_map[fqcn] = (fqcn, full_path)
         return class_map
 
     def extract_classes_from_file(self, file_path):
@@ -116,26 +117,28 @@ class DynamicClassSearchAndImportCommand(sublime_plugin.TextCommand):
         def scroll_to_class():
             if view.is_loading():
                 sublime.set_timeout(scroll_to_class, 100)
-            elif class_region := view.find(r'(class|interface|trait)\s+\w+', 0):
-                view.show_at_center(class_region)
-                view.sel().clear()
-                view.sel().add(class_region.begin())
+            else:
+                class_region = view.find(r'(class|interface|trait)\s+\w+', 0)
+                if class_region:
+                    view.show_at_center(class_region)
+                    view.sel().clear()
+                    view.sel().add(class_region.begin())
 
         sublime.set_timeout(scroll_to_class, 100)
 
 class InsertUseStatementCommand(sublime_plugin.TextCommand):
     def run(self, edit, class_name):
-        if use_region := self.view.find(r'^\s*use\s+.*?;', 0):
+        use_region = self.view.find(r'^\s*use\s+.*?;', 0)
+        if use_region:
             insert_point = use_region.end() + 1
         else:
             insert_point = self.find_namespace_end()
 
-        self.view.insert(edit, insert_point, f'use {class_name};\n')
+        self.view.insert(edit, insert_point, 'use %s;\n' % class_name)
 
     def find_namespace_end(self):
-        if ns_region := self.view.find(r'<\?php|\bnamespace\b', 0):
-            return self.view.line(ns_region).end()
-        return 0
+        ns_region = self.view.find(r'<\?php|\bnamespace\b', 0)
+        return self.view.line(ns_region).end() if ns_region else 0
 
 def plugin_loaded():
-    print("PHP Class Navigator ready (⌘+Click enabled)")
+    print("PHP Class Navigator loaded (Python 3.3 compatible)")
